734c82e9b202f7ca6fc32398cac467b0b1d60a30
gouravkhunger <gouravkhunger18@gmail.com>
2022-05-16 08:22:05 +0000

    Update gh-pages to output generated at aa7944b

---
+++ assets/js/theme.js
@@ -0,0 +1,119 @@
+window.onscroll = () => {
+  setStickyness();
+};
+
+const navbar = document.getElementById("navbar");
+const banner = document.getElementById("banner");
+const sticky = navbar.offsetTop;
+
+const themeToggleIcon = document.getElementById("theme-toggle-icon");
+
+if (localStorage.getItem("rotated") === "true") {
+  themeToggleIcon.style.transform = "rotate(180deg)";
+} else {
+  themeToggleIcon.style.transform = "rotate(0deg)";
+}
+
+const setStickyness = () => {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("fixed");
+    banner.classList.add("mt-16");
+  } else {
+    navbar.classList.remove("fixed");
+    banner.classList.remove("mt-16");
+  }
+};
+
+const toggleNav = () => {
+  const menuOpen = document.getElementById("menu-open");
+  const menuClose = document.getElementById("menu-close");
+  const mobileNav = document.getElementById("mobile-nav");
+
+  if (menuClose.classList.contains("hidden")) {
+    // nav is opened
+    menuOpen.classList.add("hidden");
+    menuClose.classList.remove("hidden");
+    mobileNav.classList.remove("hidden");
+  } else {
+    // nav is closed
+    menuOpen.classList.remove("hidden");
+    menuClose.classList.add("hidden");
+    mobileNav.classList.add("hidden");
+  }
+};
+
+const showSearch = () => {
+  document.getElementById("search-container").classList.remove("hidden");
+  document.getElementById("search-input").focus();
+  const sjs = SimpleJekyllSearch({
+    searchInput: document.getElementById("search-input"),
+    resultsContainer: document.getElementById("results-container"),
+    json: "/search.json",
+    debounceTime: 500,
+    noResultsText: "<span class='mt-2 flex'>No results found</span>",
+    searchResultTemplate: `<a class="search-item underline-none hover:border-{category}" href="{url}"><span>{title}</span></a>`,
+  });
+};
+
+const hideSearch = () => {
+  document.getElementById("search-container").classList.add("hidden");
+  document.getElementById("search-input").value = "";
+  document.getElementById("results-container").innerHTML = "";
+};
+
+// Detect keypress
+// CMD + K -> Open search
+// CTRL + K -> Open search
+// ESC -> Hide search
+document.addEventListener("keydown", function (e) {
+  if (e.keyCode === 75 && (e.ctrlKey || e.metaKey)) {
+    showSearch();
+  }
+
+  if (e.keyCode === 27) {
+    hideSearch();
+  }
+});
+
+const toggleTheme = () => {
+  if (localStorage.getItem("color-theme")) {
+    if (localStorage.getItem("color-theme") === "light") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    }
+  } else {
+    // if NOT set via local storage previously
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    }
+  }
+
+  if (localStorage.getItem("rotated") === "true") {
+    themeToggleIcon.style.transform = "rotate(0deg)";
+    localStorage.setItem("rotated", "false");
+  } else {
+    themeToggleIcon.style.transform = "rotate(180deg)";
+    localStorage.setItem("rotated", "true");
+  }
+};
+
+document.addEventListener("keydown", function (e) {
+  if ((e.ctrlKey || e.metaKey) && e.shiftKey && e.keyCode === 76) {
+    toggleTheme();
+  }
+});
+
+document
+  .getElementById("search-container")
+  .addEventListener("click", (event) => {
+    if (event.target.id === "search-container") {
+      hideSearch();
+    }
+  });
